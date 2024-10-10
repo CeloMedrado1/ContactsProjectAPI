7894042861abf15c18d5db898679eb50ff96b017
Marcelo Medrado <celo1402ang@hotmail.com>
2024-10-10 20:40:48 -0300

    Commit all 1

--- ContactsAPI/Program.cs
+++ ContactsAPI/Program.cs
@@ -1,6 +1,11 @@
+using ContactsAPI.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton<ContatoDbContext>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
